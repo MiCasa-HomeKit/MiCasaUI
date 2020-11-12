2d3ea6e85039de4ce323cd5217419021280b36b4
Thomas Bonk <thomas@meandmymac.de>
2020-11-12 12:49:27 +0100

    added SwiftUI5 dependency

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
     // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/MiCasa-HomeKit/MiCasaPlugin.git", .branch("main")),
     .package(url: "https://github.com/MiCasa-HomeKit/HAP.git", .branch("master")),
-    .package(url: "https://github.com/MiCasa-HomeKit/SwiftWebUI.git", .branch("develop")),
+    .package(url: "https://github.com/MiCasa-HomeKit/SwiftUI5.git", .branch("master")),
     .package(url: "https://github.com/realm/SwiftLint.git", from: "0.40.3")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
-    .target(name: "MiCasaUI", dependencies: ["MiCasaPlugin", "HAP", "SwiftWebUI"]),
-    .testTarget(name: "MiCasaUITests", dependencies: ["MiCasaUI"]),
+    .target(name: "MiCasaUI", dependencies: ["MiCasaPlugin", "HAP", "SwiftUI5"]),
+    .testTarget(name: "MiCasaUITests", dependencies: ["MiCasaUI"])
   ]
 )
